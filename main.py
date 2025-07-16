9932c875757cc1a03f78730d2fb1a3a41b08e61c
SoyIvito <ivannovotny6@gmail.com>
2025-07-16 19:46:35 -0300

    se agrego la actividad n2

--- main.py
+++ main.py
@@ -9,3 +9,20 @@ from funciones import redondear
 
 Aredondear = float(input("Ingrese el numero a redondear: "))
 print(redondear(Aredondear))
+
+
+"""
+2. Coloque el módulo del ejercicio anterior dentro de un paquete. (funciones.py)
+En un módulo que esté fuera de ese paquete, 
+cree una función de suma de decimales que redondee el resultado haciendo uso de la función
+redondear() del paquete recién creado.
+"""
+
+from funciones import redondear
+def suma_redondeo(a, b):
+    resultado = a + b
+    return redondear(resultado)
+
+a = float(input("Ingrese el primer numero decimal: "))
+b = float(input("Ingrese el segundo numero decimal: "))
+print(suma_redondeo(a,b))
